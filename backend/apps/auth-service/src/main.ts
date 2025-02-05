29d7a5def5b8b0ae4d34e779349ac34353a45fd4
MostafaOsama <mostafaos2021fp@gmail.com>
2025-02-05 14:48:55 +0200

    Refactor Docker configurations and integrate RabbitMQ for microservices communication

--- backend/apps/auth-service/src/main.ts
+++ backend/apps/auth-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@rabbitmq:5672'], // RabbitMQ URL from Docker
+        queue: 'auth_queue', // Define a queue for auth-service
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  Logger.warn('Auth-Service is running...');
+
+  await app.listen();
 }
 bootstrap();
